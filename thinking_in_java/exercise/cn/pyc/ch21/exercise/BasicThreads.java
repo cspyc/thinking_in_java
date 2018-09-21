f25713f52d977a18d5a6a7fbbf9edfe4e2f00d6e
piyic <piyicheng001@qq.com>
2018-09-21 08:16:28 +0800

    20180921commit

---
+++ thinking_in_java/exercise/cn/pyc/ch21/exercise/BasicThreads.java
@@ -0,0 +1,10 @@
+package cn.pyc.ch21.exercise;
+
+public class BasicThreads {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread t = new Thread(new LiftOff());
+	}
+
+}
