c684790347481fb734db1e1f57002dfb484f4849
pyc <pyc@winning.com.cn>
2020-09-17 19:03:29 +0800

    concurency

--- thinking_in_java/main/cn/pyc/ch21/exercise/concurrency/RunnableTool/CachedThreadPool.java
+++ thinking_in_java/main/cn/pyc/ch21/exercise/concurrency/RunnableTool/CachedThreadPool.java
@@ -10,7 +10,8 @@ import java.util.stream.IntStream;
  */
 public class CachedThreadPool {
     public static void main(String[] args) {
-        //当你运行这个程序时，你会发现它完成得更快。这是有道理的，每个任务都有自己的线程，所以它们都并行运行，而不是使用相同的线程来顺序运行每个任务。
+        //当你运行这个程序时，你会发现它完成得更快。
+        // 这是有道理的，每个任务都有自己的线程，所以它们都并行运行，而不是使用相同的线程来顺序运行每个任务。
         ExecutorService exec = Executors.newCachedThreadPool();
         IntStream.range(0,10)
                 .mapToObj(NapTask::new)
