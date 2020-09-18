3c13bfb5afad6459c48185f86be1884f1e3f199c
pyc <pyc@winning.com.cn>
2020-09-18 17:05:46 +0800

    'concurency'

---
+++ thinking_in_java/main/cn/pyc/ch21/exercise/concurrency/LowCurrency/DeadLockCase.java
@@ -0,0 +1,78 @@
+package cn.pyc.ch21.exercise.concurrency.LowCurrency;
+
+/**
+ * @author pi
+ * @date 2020/9/18 10:56:16
+ */
+public class DeadLockCase {
+    //创建资源
+    private static Object resourceA = new Object();
+    private static Object resourceB = new Object();
+
+    public static void main(String[] args) throws Exception {
+        //创建线程A
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (resourceA) {
+                    System.out.println(Thread.currentThread() + " get ResourceA");
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread() + " waiting get resourceB");
+
+                    //释放资源
+                    try{
+                        System.out.println("Releasing resourceA......");
+                        resourceA.wait();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                    synchronized (resourceB) {
+                        System.out.println(Thread.currentThread() + " get resourceB");
+                    }
+                }
+            }
+        });
+
+        //创建线程B
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (resourceB) {
+                    System.out.println(Thread.currentThread() + " get ResourceB");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread() + " waiting get resourceA");
+
+                    //释放资源
+                    try{
+                        System.out.println("Releasing resourceB......");
+                        resourceB.wait();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    synchronized (resourceA) {
+                        System.out.println(Thread.currentThread() + " get ResourceA");
+                    }
+                }
+            }
+        });
+
+        //启动线程
+        threadA.start();
+        threadB.start();
+
+        threadA.join();
+        threadB.join();
+
+        System.out.println("main thread over");
+    }
+}
